618ab5965750d009ee7e0089d3011fe08243eb6e
Steven perry <Sperry3006@gmail.com>
2022-10-31 19:12:46 +0000

    Completed most of the cypress testing

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,10 +1,60 @@
+/* eslint-disable no-undef */
 describe("Calculator", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-  })
+  });
 
-  it('should have working number buttons', () => {
-    cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
-  })
-})
+  it("should have working number buttons", () => {
+    cy.get("#number2").click();
+    cy.get(".display").should("contain", "2");
+  });
+
+  it("The number buttons should update the display of the running total", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number1").click();
+    cy.get(".display").should("contain", "1");
+  });
+
+  it("the arithmetical operations should update the display with the result of the operation", () => {
+    cy.get("#number4").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number3").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "12");
+  });
+
+  it("Multiple opeartions should be able to be chained together", () => {
+    cy.get("#number4").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number3").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number4").click();
+    cy.get("#operator_add").click();
+    cy.get("#number3").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "11");
+  });
+
+  it("Should be able to display decimal numbers", () => {
+    cy.get("#number2").click();
+    cy.get("#decimal").click();
+    cy.get("#number4").click();
+    cy.get("#operator_add").click();
+    cy.get("#number7").click();
+    cy.get("#decimal").click();
+    cy.get("#number1").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "9.5");
+  });
+
+  it("Should be able to display negative numbers", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number4").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "-2");
+  });
+
+  xit("How does the code perform in exceptional circumstances", () => {});
+});
